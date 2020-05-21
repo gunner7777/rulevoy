84b8a9bf5bac3cc7fb0dcc657529d3a43506197a
gunner7777 <gunner1990@yandex.ru>
2020-05-21 22:44:33 +0300

    add input style

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,42 +1,44 @@
 function TopscrollTo() {
   if (window.scrollY != 0) {
-    setTimeout(function() {
+    setTimeout(function () {
       window.scrollTo(0, window.scrollY - 300);
       TopscrollTo();
     }, 20);
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  const firstBlock = document.querySelector(".About");
-  const firstBlockHeight = firstBlock.clientHeight;
-  const headerNode = document.querySelector(".Header-Outer");
+document.addEventListener("DOMContentLoaded", function () {
+  const firstBlockHeight = 600;
   const goToUp = document.querySelector(".GoToUp");
 
-  fixedHeader(headerNode, firstBlockHeight);
   goToUpButton(goToUp, firstBlockHeight);
 
-  window.addEventListener("scroll", function() {
-    fixedHeader(headerNode, firstBlockHeight);
+  window.addEventListener("scroll", function () {
     goToUpButton(goToUp, firstBlockHeight);
   });
 
-  document.addEventListener("click", function(e) {
+  document.addEventListener("click", function (e) {
     if (e.target.classList.contains("Menu-Link")) {
       e.preventDefault();
       let blockForScroll = document.getElementById(e.target.dataset.toBlockId);
       blockForScroll.scrollIntoView({ block: "start", behavior: "smooth" });
     }
   });
-});
 
-function fixedHeader(h, fbh) {
-  if (pageYOffset > fbh - h.clientHeight) {
-    h.classList.add("Header_Fixed");
-  } else {
-    h.classList.remove("Header_Fixed");
-  }
-}
+  const inputs = document.querySelectorAll(".InputFile");
+  Array.prototype.forEach.call(inputs, function (input) {
+    let label = input.nextElementSibling;
+    console.log("label", label.textContent);
+    let labelVal = label.innerHTML;
+    input.addEventListener("change", function (e) {
+      let fileName = "";
+      fileName = e.target.value.split("\\").pop();
+
+      if (fileName) label.innerHTML = fileName;
+      else label.innerHTML = labelVal;
+    });
+  });
+});
 
 function goToUpButton(gtu, fbh) {
   if (pageYOffset > fbh) {
